1395310150c03a1441593f2b7a2e8a86b485f2fe
christianzaboli <zabolichristian@gmail.com>
2025-08-27 17:12:21 +0200

    dettagli e commenti extra

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
 
 // chiediamo all'utente la sua etá
@@ -16,24 +16,32 @@ const userKmDistance = prompt ('Quanti Chilometri devi percorrere?')
 console.log(parseInt(userKmDistance));
 
 // variabili necessarie per continuare il programma
-let finalTicketPrice;
-const kmDistance = 1
-const flatKmTariff = (0.21 * kmDistance)
-const underageDiscount = (20 / 100)
-const elderlyDiscount = (40 / 100)
+let discountTicketPrice;
+const kmDistance = 1;
+const flatKmTariff = (0.21 * kmDistance);
+const underageDiscount = (20 / 100);
+const elderlyDiscount = (40 / 100);
 
 // check di quanto costerá il biglietto senza sconti
-let baseTicketPrice = (userKmDistance * flatKmTariff);
+let baseTicketPrice = ((userKmDistance * flatKmTariff)).toFixed(2);
 console.log(baseTicketPrice);
 
 // applichiamo uno sconto del 20% nel caso dovesse essere minorenne
 if (userAge < 18)  {
-    let finalTicketPrice = (baseTicketPrice - (underageDiscount * baseTicketPrice)).toFixed(2)
-    console.log(finalTicketPrice) 
+    let discountTicketPrice = (baseTicketPrice - (underageDiscount * baseTicketPrice)).toFixed(2)
+    console.log(discountTicketPrice);
+    let priceAlert = alert('il tuo biglietto costerá: ' + discountTicketPrice + ' €')
 } 
 
 // applichiamo uno sconto del 40% nel caso dovesse essere over 65
-else if (userAge > 65) {
-    let finalTicketPrice = (baseTicketPrice - (elderlyDiscount * baseTicketPrice)).toFixed(2)
-    console.log(finalTicketPrice)
+else if (userAge > 65 ) {
+    let discountTicketPrice = (baseTicketPrice - (elderlyDiscount * baseTicketPrice)).toFixed(2)
+    console.log(discountTicketPrice)
+    let priceAlert = alert('il tuo biglietto costerá: ' + discountTicketPrice + ' €')
 }
+
+// infine il biglietto nel resto dei casi
+else {
+    let priceAlert = alert('il tuo biglietto costerá: ' + baseTicketPrice + ' €')
+}
+
